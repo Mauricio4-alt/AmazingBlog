62250f7b3509e99bd702b8182ae08336330f26b4
Mauricio Urdaneta <mauriurdaneta20@gmail.com>
2025-04-21 21:05:46 -0500

    creando metodo post con autenticación de token

---
+++ routes/blog.py
@@ -0,0 +1,19 @@
+from flask import Blueprint,request,jsonify
+from flask_jwt_extended import jwt_required,get_jwt_identity
+from models import BlogPost,db, Users
+
+blog_bp_routh = Blueprint('blog',__name__)
+
+@blog_bp_routh('/post',methods=['POST'])
+@jwt_required
+def create_post():
+    data = request.get_json()
+    title = data.get('title')
+    content = data.get('content')
+    user_id = get_jwt_identity()
+    if not title or not content:
+        return jsonify({'msg':'Title and content required'}),400
+    post =BlogPost(title=title,content=content,user_id=user_id)
+    db.session.add(post)
+    db.session.commit()
+
