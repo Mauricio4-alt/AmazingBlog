0e68b2e74fcfa1a6349fccf4f5a80f006ae6ca6b
Mauricio Urdaneta <mauriurdaneta20@gmail.com>
2025-04-21 22:23:41 -0500

    solucionando error de llamada con ruta

--- routes/blog.py
+++ routes/blog.py
@@ -4,7 +4,7 @@ from models import BlogPost,db, Users
 
 blog_bp_routh = Blueprint('blog',__name__)
 
-@blog_bp_routh('/post',methods=['POST'])
+@blog_bp_routh.route('/post',methods=['POST'])
 @jwt_required()
 def create_post():
     data = request.get_json()
@@ -13,6 +13,7 @@ def create_post():
     user_id = get_jwt_identity()
     if not title or not content:
         return jsonify({'msg':'Title and content required'}),400
+    
     post =BlogPost(title=title,content=content,user_id=user_id)
     db.session.add(post)
     db.session.commit()
